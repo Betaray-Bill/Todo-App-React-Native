b41a3d5882d2b3d11675b383f27efe4c85866654
Betaray-Bill <suryaorion12@gmail.com>
2021-11-04 14:17:45 +0530

    Adding List

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setenteredGoal] = useState("")
+  const [goals, setgoals] = useState([])
+
+  const goalInputHandler = (e) => {
+    setenteredGoal(e)
+  }
+
+  const addGoal = () => {
+    setgoals([...goals ,{id:Math.random().toString()  , value:enteredGoal}])
+    setenteredGoal(" ")
+  }
+
+  const removeGoal = (g_del) => {
+    setgoals(
+      e => {
+        return e.filter((goal) => goal.id !== g_del )
+      }
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Text>Todo App</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={{borderColor:'black', borderWidth:1, padding:8, width:"80%"}} 
+          placeholder="Today's Goals..."
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button 
+          style={{marginTop:2}} 
+          title={"Add"}
+          onPress={addGoal}
+        />
+      </View>
+      <View> 
+        <ScrollView vertical> 
+          {
+            goals.map((goal, _i) => <TouchableOpacity style={styles.listItems} onPress={removeGoal} ><Text key={goal}>{_i+1} - {goal}</Text></TouchableOpacity>)
+          }
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding:20,
+    paddingTop: 100
   },
+  inputContainer:{
+    flexDirection:"row",
+    alignItems: 'center',
+    justifyContent:"center"
+  },
+  listItems:{
+    padding:10,
+    backgroundColor:"#ccc",
+    borderColor:'black',
+    borderWidth:1,
+    marginVertical:4
+  }
 });
